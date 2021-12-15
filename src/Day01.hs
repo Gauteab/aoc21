c914be0d086afa234c03e784a72ea4c05e7eabb8
Gaute Berge <gaute.berge@gmail.com>
2021-12-15 20:20:14 +0100

    improve day1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,13 +1,11 @@
-{-# LANGUAGE TypeApplications #-}
-
 module Day01 (
   main,
 ) where
 
-import Relude.Unsafe (fromJust)
+import Control.Lens
 
 main :: IO ()
 main = do
-  input <- map (read @Int) . lines <$> readFile "input/01"
+  input <- map read . lines <$> readFile "input/01"
   print . length . filter (uncurry (<)) . (zip <*> tail) $ input
-  print . length . filter (uncurry (<)) . (zip <*> tail) . map (\(a, b, c) -> a + b + c) $ zip3 input (tail input) (drop 2 input)
+  print . length . filter (uncurry (<)) . (zip <*> tail) . map (sum . toListOf each) . (zip3 <*> tail <*> drop 2) $ input
